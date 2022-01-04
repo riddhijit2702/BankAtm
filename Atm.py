3134e6c2ac13f0e08a5bdb484c4aca36648b9de1
spaceCrazy <74096278+riddhijit2702@users.noreply.github.com>
2022-01-04 14:55:03 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,43 @@
+from abc import abstractclassmethod
+from typing import Mapping
+
+
+class Atm(object):
+    # Considering the variables
+   
+
+    def __init__(self,cardNumber,pinNumber):
+        self.cardNumber=cardNumber
+        self.pinNumber=pinNumber
+     
+    
+    def cardNumberCheck(self): 
+        x = input("Enter it here")
+        print("Your Balance is "+ x)
+    
+    def withdrawl(self,amount):
+        x = input("Enter it here")
+        new_amount = x - amount
+        print("you have withdrawn amount "+str(amount) +". Your remaining balance is "+ str(new_amount))
+def main():
+        cN = input("Enter your Card Number")
+        pin = input("Enter the Pin Number")
+
+        user = Atm(cN,pin)
+
+        print("Please mention the type of actvity you want to proceed with")
+        print("1.Balance Enquriy  ; 2.withdrawl")
+        
+        activity = input("Enter Activity number here")
+        
+        if(activity == 1):
+           user.cardNumberCheck()
+        elif (activity == 2):
+            amount = int(input("enter the amount:- "))
+            user.withdrawl(amount)
+        else:
+            print("Invalid Number")
+
+
+if __name__ == "__main__":
+    main()
